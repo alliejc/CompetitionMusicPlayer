d9f1416f722313fa172b7820b4dec205aa28e4f8
Alisonjc <alison.jane.c@gmail.com>
2016-02-11 17:24:12 -0800

    Creating PlaylistTracksList Java

---
+++ app/src/main/java/com/example/alisonjc/compmusicplayer/PlaylistTracksList.java
@@ -0,0 +1,13 @@
+package com.example.alisonjc.compmusicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaylistTracksList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist_tracks_list);
+    }
+}
