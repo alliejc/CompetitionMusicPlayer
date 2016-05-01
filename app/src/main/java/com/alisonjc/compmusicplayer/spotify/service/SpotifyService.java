f0afd6ec0120965ae6b2d71f80c8b410ed0211e8
Alisonjc <alison.jane.c@gmail.com>
2016-05-01 15:14:03 -0700

    icon change

---
+++ app/src/main/java/com/alisonjc/compmusicplayer/spotify/service/SpotifyService.java
@@ -0,0 +1,23 @@
+package com.alisonjc.compmusicplayer.spotify.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class SpotifyService {
+
+    private final SpotifyServiceInterface mSpotifyService;
+
+    public SpotifyService() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.spotify.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        mSpotifyService = retrofit.create(SpotifyServiceInterface.class);
+    }
+    public SpotifyServiceInterface getSpotifyService() {
+        return mSpotifyService;
+    }
+}
