2d0810863c95d35d38f24c60ac344dafa5c4ed76
Alison Caldwell <acaldwell@webmd.net>
2018-02-02 01:43:39 -0500

    add a playlist added to recycler view

--- app/src/main/java/com/alisonjc/compmusicplayer/viewholder/AddPlaylistViewHolder.java
+++ app/src/main/java/com/alisonjc/compmusicplayer/viewholder/AddPlaylistViewHolder.java
@@ -12,13 +12,9 @@ import com.alisonjc.compmusicplayer.databinding.TrackItemModel;
 import com.alisonjc.compmusicplayer.spotify.spotify_model.PlaylistModel.Item;
 import com.squareup.picasso.Picasso;
 
-/**
- * Created by acaldwell on 2/2/18.
- */
 
 public class AddPlaylistViewHolder extends RecyclerView.ViewHolder {
     public TextView playlistTitle;
-    public TextView songCount;
     public ImageView image;
     public View itemView;
 
@@ -26,19 +22,14 @@ public class AddPlaylistViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
         this.itemView = itemView;
 
-        playlistTitle = (TextView) itemView.findViewById(R.id.playlist_header_text);
-        songCount = (TextView) itemView.findViewById(R.id.playlist_sub_text);
-        image = (ImageView) itemView.findViewById(R.id.playlist_image);
+        playlistTitle = (TextView) itemView.findViewById(R.id.add_a_playlist_header_text);
+        image = (ImageView) itemView.findViewById(R.id.add_a_playlist_image);
     }
 
     public void bind(final Item item, final PlaylistAdapter.onItemClickListener listener) {
         if(item != null){
                 playlistTitle.setText(item.getName());
                 Picasso.with(itemView.getContext()).load(R.drawable.ic_add_copy).error(R.drawable.ic_menu_gallery).into(image);
-
-            if(item.getTracks() != null){
-                songCount.setText(item.getTracks().getTotal().toString() + " songs");
-            }
         }
 
         itemView.setOnClickListener(view ->
